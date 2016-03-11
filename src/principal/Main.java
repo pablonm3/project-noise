2f9ba8f19dafc353aafbada375a8b5430058db1d
pablonm3 <pablonicolasm.pm@gmail.com>
2016-03-11 14:17:01 -0300

    added random comment vettings/

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
 	public static void main(String args[])
 	{
+		// RANDOM COMMENT
 		Cola cola;
 		JFileChooser chooser = new JFileChooser();
 		chooser.setMultiSelectionEnabled(true);
